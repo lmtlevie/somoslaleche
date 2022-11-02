6de0d03f24b278e735fb9b43ab18b8c330a498bd
lmtlevie <luciano.levy@7puentes.com>
2022-11-02 10:25:53 -0300

    Clasificador fuentes

--- milk_scrapper.py
+++ milk_scrapper.py
@@ -1,9 +1,33 @@
 import requests
 from lxml import etree
 import io
+import re;
 
 
-def scrap_milks_html(data,list_xpath,title_xpath,price_xpath,pager=None):
+def clasificador(leche):
+    categoria = [];
+
+    if(re.match(r"polvo",leche,re,re.IGNORECASE)):
+        categoria.append("Polvo")
+    if(re.match(r"(vegetal|almendras|planta|a base de))",leche,re,re.IGNORECASE)):
+        categoria.append("Vegetal")
+    if(re.match(r"(descremada",leche,re,re.IGNORECASE)):
+        categoria.append("Descremada")
+    if(re.match(r"entera",leche,re,re.IGNORECASE)):
+        categoria.append("Entera")
+    if(re.match(r"(bebe|infantil|bebé)",leche,re,re.IGNORECASE)):
+        categoria.append("Infantil")
+    if(re.match(r"larga vida",leche,re,re.IGNORECASE)):
+        categoria.append("Larga Vida")
+    if(re.match(r"sachet",leche,re,re.IGNORECASE)):
+        categoria.append("Sachet")
+    if(re.match(r"(chocolatada|chocolate)",leche,re,re.IGNORECASE)):
+        categoria.append("Saborizadas")
+    if(len(categoria) == 0):
+        categoria.append("Otro")
+    return categoria
+
+def scrap_milks_html(data,list_xpath,title_xpath,price_xpath,writer,pager=None):
     res = requests.get(data["url"])
     html_doc = io.StringIO(res.text)
     parser = etree.HTMLParser()
@@ -11,13 +35,13 @@ def scrap_milks_html(data,list_xpath,title_xpath,price_xpath,pager=None):
     for p in tree.xpath(list_xpath):
         title = p.xpath(title_xpath)
         price = p.xpath(price_xpath)
-        #categoria = clasificador(p)
-        #subir_data()
+        categoria = clasificador(p)
+        writer.writerow([title,data[]])
 
     if pager is not None:
         for page in tree.xpath(pager["pager_xpath"]):
             page_url = page.xpath(pager["url_xpath"])
-            scrap_milks_html({"url":page_url[0]},list_xpath,title_xpath,price_xpath)
+            scrap_milks_html({"url":page_url[0]},list_xpath,title_xpath,price_xpath,writer)
 
     # tirar los valores a la base
 
--- milking_fonts.py
+++ milking_fonts.py
@@ -1,11 +1,16 @@
 from milk_scrapper import scrap_milks_html
 import json
+import csv
 
 with open('fonts.json') as f:
    data = json.load(f)
 
-scrap_milks_html(data["Condor"],"//li[.//h2[@class='woocommerce-loop-product__title']]",".//h2/text()",".//span[@class='price']//bdi/text()",{"pager_xpath":"//ul[@class='page-numbers']/li[./a[@class='page-numbers']]","url_xpath":"./a/@href"})
+with open("result.csv") as res:
+   writer = csv.writer(f)
 
-scrap_milks_html(data["Tu Almacen"],"//div[@class='item']","./p/text()","./div[@class='precio']/text()")
 
-scrap_milks_html(data["Alvear"],"//div[@class='product-small box ']","./div[@class='title-wrapper']//a/text()")
+scrap_milks_html(data["Condor"],"//li[.//h2[@class='woocommerce-loop-product__title']]",".//h2/text()",".//span[@class='price']//bdi/text()",writer,{"pager_xpath":"//ul[@class='page-numbers']/li[./a[@class='page-numbers']]","url_xpath":"./a/@href"})
+
+scrap_milks_html(data["Tu Almacen"],"//div[@class='item']","./p/text()","./div[@class='precio']/text()",writer)
+
+scrap_milks_html(data["Alvear"],"//div[@class='product-small box ']","./div[@class='title-wrapper']//a/text()",writer)
