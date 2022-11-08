9cdf264913a2b04e3e01d61c3465fb1dd04dee57
lmtlevie <luciano.levy@7puentes.com>
2022-11-08 15:07:00 -0300

    Integracion con bigquery

--- scrapper/milk_scrapper.py
+++ scrapper/milk_scrapper.py
@@ -1,17 +1,53 @@
 import requests
 from lxml import etree
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
 import io
 import re
 
+def create_table_bigquery(client,table_id) -> None:
+    schema = [
+            bigquery.SchemaField("title", "STRING", mode="REQUIRED"),
+            bigquery.SchemaField("seller", "STRING", mode="REQUIRED"),
+            bigquery.SchemaField("price", "INTEGER", mode="REQUIRED"),
+            bigquery.SchemaField("category", "STRING", mode="REQUIRED"),
+            bigquery.SchemaField("location", "STRING", mode="REQUIRED"),
+            ]
+
+    table = bigquery.Table(table_id, schema=schema)
+    table = client.create_table(table)
+    print(
+        "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+    )
+
+def add_row_bigquery(row:dict) -> None:
+    PROJECT = "alumnos-sandbox"
+    DATASET = "precios_productos"
+    TABLE = "grupo_3"
+    client = bigquery.Client(project=PROJECT)
+    table_id = f"{PROJECT}.{DATASET}.{TABLE}"
+    table = None
+    try:
+        table = client.get_table(table_id)
+    except NotFound:
+        create_table_bigquery(client,table_id)
+        table = client.get_table(table_id)
+
+
+    errors = client.insert_rows_json(table, row)
+    if errors == []:
+        print("Success")
+
 
 def clasificador(titulo:str) -> list:
+    """"""
     categoria_final = []
     titulo = titulo[0]
-    categorias = {"Polvo":r"polvo","Vegetal":r"(vegetal|almendras|planta|a base de)",
-                  "Descremada":r"descremada",
-                  "Entera":r"entera","Infantil":r"(bebe|infantil|bebé)",
-                  "Larga Vida":r"larga vida",
-                  "Sachet":r"sachet",
+    categorias = {"Polvo":      r"polvo","Vegetal":r"(vegetal|almendras|planta|a base de)",
+                  "Descremada": r"descremada",
+                  "Entera":     r"entera","Infantil":r"(bebe|infantil|bebé)",
+                  "Larga Vida": r"larga vida",
+                  "Sachet":     r"sachet",
                   "Saborizadas":r"(chocolatada|chocolate)"
                 }
 
@@ -25,7 +61,8 @@ def clasificador(titulo:str) -> list:
 
     return categoria_final
 
-def scrap_milks_html(data:dict,list_xpath:str,title_xpath:str,price_xpath:str,writer,pager:dict=None) -> None:
+def scrap_milks_html(data:dict,list_xpath:str,title_xpath:str,price_xpath:str,pager:dict=None) -> None:
+    """"""
     res = requests.get(data["url"])
     html_doc = io.StringIO(res.text)
     parser = etree.HTMLParser()
@@ -35,11 +72,16 @@ def scrap_milks_html(data:dict,list_xpath:str,title_xpath:str,price_xpath:str,wr
         title = p.xpath(title_xpath)
         price = p.xpath(price_xpath)
         categoria = clasificador(title)
-        writer.writerow([title[0],"",data["name"],int(float(price[0].replace("$","").replace(",",""))),categoria[0]])
+        add_row_bigquery({"title":      title[0],"brand":"",
+                          "seller":     data["name"],
+                          "price":      int(float(price[0].replace("$","").replace(",",""))),
+                          "category":   categoria[0],
+                          "location":   data["location"]})
 
     if pager is not None:
         for page in tree.xpath(pager["pager_xpath"]):
             page_url = page.xpath(pager["url_xpath"])
-            scrap_milks_html({"url":page_url[0],"name":data["name"]},list_xpath,title_xpath,price_xpath,writer)
+            data["url"] = page_url[0]
+            scrap_milks_html(data,list_xpath,title_xpath,price_xpath,writer)
 
 
--- scrapper/milking_fonts.py
+++ scrapper/milking_fonts.py
@@ -5,12 +5,9 @@ import csv
 with open('scrapper/sources.json') as f:
    data = json.load(f)
 
-   with open("scrapper/results.csv","w") as res:
-      writer = csv.writer(res)
 
-      writer.writerow(["Producto","Marca","Mercado","Precio","Categoria"])
-      scrap_milks_html(data["Condor"],"//li[.//h2[@class='woocommerce-loop-product__title']]",".//h2/text()",".//span[@class='price']//bdi/text()",writer,{"pager_xpath":"//ul[@class='page-numbers']/li[./a[@class='page-numbers']]","url_xpath":"./a/@href"})
+   scrap_milks_html(data["Condor"],"//li[.//h2[@class='woocommerce-loop-product__title']]",".//h2/text()",".//span[@class='price']//bdi/text()",{"pager_xpath":"//ul[@class='page-numbers']/li[./a[@class='page-numbers']]","url_xpath":"./a/@href"})
 
-      scrap_milks_html(data["Tu Almacen"],"//div[@class='item']","./p/text()","./div[@class='precio']/text()",writer)
+   scrap_milks_html(data["Tu Almacen"],"//div[@class='item']","./p/text()","./div[@class='precio']/text()")
 
-      scrap_milks_html(data["Alvear"],"//div[@class='product-small box ']",".//div[@class='title-wrapper']//a/text()",".//span[@class='price']/del/span/text()",writer)
+   scrap_milks_html(data["Alvear"],"//div[@class='product-small box ']",".//div[@class='title-wrapper']//a/text()",".//span[@class='price']/del/span/text()")
