c9771608b9103a2f29e3e2e9435d5d8dfde6f93e
lmtlevie <luciano.levy@7puentes.com>
2022-11-11 11:59:34 -0300

    Cambios xpath laproveduria

--- scrapper/milking_fonts.py
+++ scrapper/milking_fonts.py
@@ -6,10 +6,10 @@ with open('scrapper/sources.json') as f:
    data = json.load(f)
 
 
-   scrap_milks_html(data["condor"],"//li[.//h2[@class='woocommerce-loop-product__title']]",".//h2/text()",".//span[@class='price']//bdi/text()",{"pager_xpath":"//ul[@class='page-numbers']/li[./a[@class='page-numbers']]","url_xpath":"./a/@href"})
+   #scrap_milks_html(data["condor"],"//li[.//h2[@class='woocommerce-loop-product__title']]",".//h2/text()",".//span[@class='price']//bdi/text()",{"pager_xpath":"//ul[@class='page-numbers']/li[./a[@class='page-numbers']]","url_xpath":"./a/@href"})
 
-   scrap_milks_html(data["tu_almacen"],"//div[@class='item']","./p/text()","./div[@class='precio']/text()")
+   #scrap_milks_html(data["tu_almacen"],"//div[@class='item']","./p/text()","./div[@class='precio']/text()")
 
-   scrap_milks_html(data["alvear"],"//div[@class='product-small box ']",".//div[@class='title-wrapper']//a/text()",".//span[@class='price']/del/span/text()")
+   #scrap_milks_html(data["alvear"],"//div[@class='product-small box ']",".//div[@class='title-wrapper']//a/text()",".//span[@class='price']/del/span/text()")
 
-   scrap_milks_html(data["la_proveduria"],"//div[@class='products list']/div/div//div[@class='row']",".//h3/text()",".//div[@class='price']/text()",{"pager_xpath":"//div[@class='btn-group']","url_xpath":".//a/@href","host":"https://www.laproveeduria.ar"})
+   scrap_milks_html(data["la_proveduria"],"//div[@class='item']",".//h3/text()",".//div[@class='price']/text()",{"pager_xpath":"//div[@class='btn-group']","url_xpath":".//a/@href","host":"https://www.laproveeduria.ar"})
